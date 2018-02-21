1d69e973ad795260bd04f5e782cb12da748b34c9
sbal13 <sbal13@gmail.com>
2018-02-21 13:26:26 -0500

    Update MenuBar.js

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -16,19 +16,19 @@ const MenuBar = (props) => {
 	return (
       <div className="ui four item menu">
             <a className="item active" id="profile">
-            	<i className="user large icon"/>
+            	<i className="user large icon" id="profile"/>
             </a>
 
             <a className="item" id="photo">
-            	<i className="photo large icon"/>
+            	<i className="photo large icon" id="photo"/>
             </a>
 
             <a className="item" id="cocktail">
-            	<i className="cocktail large icon"/>
+            	<i className="cocktail large icon" id="cocktail"/>
             </a>
 
             <a className="item" id="pokemon"> 
-            	<i className=" themeisle large icon"/>
+            	<i className=" themeisle large icon" id="pokemon"/>
             </a>
       </div>
       )
