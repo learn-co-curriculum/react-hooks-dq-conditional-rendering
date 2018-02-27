9d4e6ab6c68225a83f2c25e27420ba7cfd662202
sbal13 <sbal13@gmail.com>
2018-02-27 16:16:25 -0500

    changed tabs to spaces

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -5,23 +5,23 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 class MainBox extends React.Component {
 
 
-	render (){
+  render (){
 
-		/* 
+    /* 
 
-		Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
+    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
 
-		*/
+    */
 
-		let detailsToDisplay = <div>Hi, I'm a div!</div> 
+    let detailsToDisplay = <div>Hi, I'm a div!</div> 
 
-		return (
-			<div>
-				<MenuBar />
-				{detailsToDisplay}
-			</div>
-		)
-	}
+    return (
+      <div>
+        <MenuBar />
+        {detailsToDisplay}
+      </div>
+    )
+  }
 
 }
 
