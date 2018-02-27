67b76f14e9156595d7a793b4876b352ca1e791ba
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-02-27 16:28:50 -0500

    formatting && brevity

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -4,51 +4,38 @@ import {Card, Button, Image, Loader} from 'semantic-ui-react'
 
 /*No need to code in here! These are merely presentational components.*/
 
-export const Profile = () => {
-
-  return (
-    <Card fluid>
-      <Card.Content textAlign="center">
-        <h3>Hi, my name is {userData.name}</h3>
-        <h5>A little about me: </h5>
-        <p>{userData.description}</p>
-        <p>Favorite phrase: {userData.catchphrase}</p>
-        <p>Age: {userData.age}</p>
-      </Card.Content>
-    </Card>
-  )
-}
+export const Profile = () => (
+  <Card fluid>
+    <Card.Content textAlign="center">
+      <h3>Hi, my name is {userData.name}</h3>
+      <h5>A little about me: </h5>
+      <p>{userData.description}</p>
+      <p>Favorite phrase: {userData.catchphrase}</p>
+      <p>Age: {userData.age}</p>
+    </Card.Content>
+  </Card>
+)
 
 
-
-export const Photos = () => {
-
-  return (
-    <Card fluid>
-      <Card.Content textAlign="center">
-        <h2>{photo.title}</h2>
-        <Image src={photo.url}/>
-      </Card.Content>
-    </Card>
-  )
-
-}
-
+export const Photos = () => (
+  <Card fluid>
+    <Card.Content textAlign="center">
+      <h2>{photo.title}</h2>
+      <Image src={photo.url}/>
+    </Card.Content>
+  </Card>
+)
 
 
 export const Cocktails = () => {
 
-  const cocktailsElements = 
-    cocktails.drinks.map((drink,index) => {
-      return(
-        <li key={index}>
-        	<h5>Name: {drink.name}</h5>
-        	<p>Description: {drink.description}</p>
-        	<br/>
-        </li>
-      )
-    })
-
+  const cocktailsElements = cocktails.drinks.map((drink, idx) => (
+    <li key={idx}>
+    	<h5>Name: {drink.name}</h5>
+    	<p>Description: {drink.description}</p>
+    	<br/>
+    </li>
+  ))
 
   return (
     <Card fluid>
@@ -59,8 +46,8 @@ export const Cocktails = () => {
       </Card.Content>
     </Card>
   )
-
 }
+
 
 export class Pokemon extends React.Component {
 
@@ -69,40 +56,39 @@ export class Pokemon extends React.Component {
   }
 
   getRandomPokemon = () => {
-
     let pokeId = Math.round(Math.random()*802)
-
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
-    .then(res => res.json())
-    .then(pokemon => this.setState({pokemon: pokemon}))
+      .then(res => res.json())
+      .then(pokemon => this.setState({pokemon: pokemon}))
   }
 
   componentDidMount() {
     this.getRandomPokemon()
   }
 
-
-  render(){
-
-
-    let content;
-
+  determineContent = () => {
     if (this.state.pokemon) {
-      content = 
+      return (
         <div>
           <h3>{this.state.pokemon.name}</h3>
           <Image src={this.state.pokemon.sprites.front_default}/>
           <br/>
           <Button onClick={this.getRandomPokemon}>Get New Pokemon</Button>
         </div>
-    } else {
-        content = <div style={{height: 300}}><Loader active size='large'>Loading</Loader></div>
+      )
     }
+    return (
+      <div style={{height: 300}}>
+        <Loader active size='large'>Loading</Loader>
+      </div>
+    )
+  }
 
+  render() {
     return (
       <Card fluid>
         <Card.Content textAlign="center">
-          {content}
+          {this.determineContent()}
         </Card.Content>
       </Card>
     )
