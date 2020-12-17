901f8879928b0ae270c9e7085fdf7b736abc6a7f
ihollander <ianholla@gmail.com>
2020-12-17 07:55:10 -0500

    Updated starter code

--- src/components/pages/Pokemon.js
+++ src/components/pages/Pokemon.js
@@ -2,24 +2,30 @@ import React, { useEffect, useState } from "react";
 import { Card, Button, Image, Loader } from "semantic-ui-react";
 
 function Pokemon() {
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState({
+    data: null,
+    isLoaded: false,
+  });
 
-  useEffect(() => {
+  useEffect(getRandomPokemon, []);
+
+  function getRandomPokemon() {
+    setPokemon({ data: null, isLoaded: false });
     const pokeId = Math.round(Math.random() * 802);
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
       .then((res) => res.json())
-      .then((pokemon) => setPokemon(pokemon));
-  }, []);
+      .then((pokemon) => setPokemon({ data: pokemon, isLoaded: true }));
+  }
 
   return (
     <Card fluid>
       <Card.Content textAlign="center">
-        {pokemon ? (
+        {pokemon.isLoaded ? (
           <div>
-            <h3>{this.state.pokemon.name}</h3>
-            <Image src={this.state.pokemon.sprites.front_default} />
+            <h3>{pokemon.data.name}</h3>
+            <Image src={pokemon.data.sprites.front_default} />
             <br />
-            <Button onClick={this.getRandomPokemon}>Get New Pokemon</Button>
+            <Button onClick={getRandomPokemon}>Get New Pokemon</Button>
           </div>
         ) : (
           <div style={{ height: 300 }}>
